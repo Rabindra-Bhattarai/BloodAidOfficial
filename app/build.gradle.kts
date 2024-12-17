42a66a51e3b9e03d1ca66e4db0d223f2b7e589e2
ChaoticAnisha <anishashah0117>
2024-12-17 08:54:00 +0545

    splash screen and background

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.blood_aid"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.blood_aid"
@@ -45,4 +45,6 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation ("com.airbnb.android:lottie:3.6.1")
+
 }
