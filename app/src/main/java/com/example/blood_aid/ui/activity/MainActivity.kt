d091eea54ae1eb6417043dacd2b66d11fa83f061
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2024-12-20 10:20:38 +0545

    Folder structure

--- app/src/main/java/com/example/blood_aid/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/blood_aid/ui/activity/MainActivity.kt
@@ -1,16 +1,21 @@
-package com.example.blood_aid
+package com.example.blood_aid.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.blood_aid.R
 
-class StartActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_start)
+        setContentView(R.layout.activity_main)
+        val intent = Intent(this@MainActivity, SplashActivity::class.java)
+        startActivity(intent)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
