59cb28e5234554acce6100160fadb5c5d65540cc
Sarjak Bhandari <bhandarisarjak@gmail.com>
2024-12-20 19:18:38 +0545

    View Binding + Click event Functions

--- app/src/main/java/com/example/blood_aid/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/example/blood_aid/ui/activity/SplashActivity.kt
@@ -1,6 +1,9 @@
 package com.example.blood_aid.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +15,9 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity,StartActivity::class.java)
+        },3000)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
