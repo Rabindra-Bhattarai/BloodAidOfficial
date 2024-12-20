141cce025aff650f83a2bdf016e724f95a7790e8
Rabindra-Bhattarai <rabindrabhattarai01@gmail.com>
2024-12-20 10:20:38 +0545

    Folder structure

--- app/src/main/java/com/example/blood_aid/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/example/blood_aid/ui/activity/SplashActivity.kt
@@ -1,10 +1,11 @@
-package com.example.blood_aid
+package com.example.blood_aid.ui.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.blood_aid.R
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
