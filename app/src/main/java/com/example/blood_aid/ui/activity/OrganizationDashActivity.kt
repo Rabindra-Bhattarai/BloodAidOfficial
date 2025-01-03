31e9b94a5b1c96f1ec925e6a1423e180184af788
ChaoticAnisha <anishashah0117>
2025-01-03 12:21:21 +0545

    NGO/Org Dashboard

--- app/src/main/java/com/example/blood_aid/ui/activity/OrganizationDashActivity.kt
+++ app/src/main/java/com/example/blood_aid/ui/activity/OrganizationDashActivity.kt
@@ -7,11 +7,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.blood_aid.R
 
-class AdminDashActivity : AppCompatActivity() {
+class OrganizationDashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
+        setContentView(R.layout.activity_organization_dash)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
         }
     }
-
+}
