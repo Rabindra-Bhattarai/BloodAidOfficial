b82cd5ea78466a26de87496cf41ffe004595324e
Sarjak <bhandarisarjak@gmail.com>
2025-01-22 10:04:56 +0545

    Some changes on the dashboard files and id defining for backend"

--- app/src/main/java/com/example/blood_aid/ui/activity/UserDashActivity.kt
+++ app/src/main/java/com/example/blood_aid/ui/activity/UserDashActivity.kt
@@ -6,12 +6,16 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.blood_aid.R
+import com.example.blood_aid.databinding.ActivityUserDashBinding
 
 class UserDashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityUserDashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding= ActivityUserDashBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_user_dash)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
