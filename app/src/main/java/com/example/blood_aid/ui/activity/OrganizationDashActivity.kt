12e25b5ea0c4920fabccf3cd859aa5a64849f563
Sarjak Bhandari <bhandarisarjak@gmail.com>
2025-02-16 13:38:15 +0545

    organization dash intents

--- app/src/main/java/com/example/blood_aid/ui/activity/OrganizationDashActivity.kt
+++ app/src/main/java/com/example/blood_aid/ui/activity/OrganizationDashActivity.kt
@@ -1,17 +1,29 @@
 package com.example.blood_aid.ui.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.blood_aid.R
+import com.example.blood_aid.databinding.ActivityOrganizationDashBinding
 
 class OrganizationDashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOrganizationDashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_organization_dash)
+        binding=ActivityOrganizationDashBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        binding.orgChg.setOnClickListener{
+            startActivity(Intent(this,OrgProfileActivity::class.java))
+        }
+        binding.evntBtn.setOnClickListener{
+            startActivity(Intent(this,EventActivity::class.java))
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
